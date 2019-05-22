eef1aa7d9a0dab07aaef2cf041de7b9094d27cd0
dafyddllyr <dafyddllyr@gmail.com>
2019-05-22 15:01:28 +0100

    Beginning CRUD implementation

---
+++ db/console.rb
@@ -0,0 +1,16 @@
+require_relative("../models/artist")
+require_relative("../models/album")
+require("pry")
+
+artist_1 = Artist.new({"name" => "Weezer"})
+artist_1.save()
+
+album_1 = Album.new({
+  "title" => "The Blue Album",
+  "genre" => "Alternative Rock",
+  "artist_id" => artist_1.id
+  })
+album_1.save()
+
+binding.pry
+nil
---
+++ models/album.rb
@@ -0,0 +1,25 @@
+require_relative("../db/sql_runner")
+
+class Album
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @title = options["title"]
+    @genre = options["genre"]
+    @artist_id = options["artist_id"]
+  end
+
+  def save()
+    sql = "INSERT INTO albums(title, genre, artist_id) VALUES ($1, $2, $3) RETURNING id"
+    values = [@title, @genre, @artist_id]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]["id"].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM albums"
+    result = SqlRunner.run(sql)
+    return result.map { |album| Album.new(album) }
+  end
+
+end
